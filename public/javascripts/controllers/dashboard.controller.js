85bf9399bb0f5332c0249982081877e7887d2ff9
Victor Gil <vgil@switch.com.uy>
2016-06-27 02:55:47 -0300

    sample links

--- public/javascripts/controllers/dashboard.controller.js
+++ public/javascripts/controllers/dashboard.controller.js
@@ -2,11 +2,24 @@
   angular.module('app')
     .controller('DashboardController', DashboardController);
 
-    DashboardController.$inject = [];
+    DashboardController.$inject = ['Venue', 'Band'];
 
-    function DashboardController(){
+    function DashboardController(Venue, Band){
       var vm = this;
       vm.title = '1,2,3,4! Dashboard';
-      
+
+      activate();
+      function activate() {
+        Band.query({}, function(bands) {
+          if (bands.length > 0) {
+            vm.band = bands[0];
+          }
+        });
+        Venue.query({}, function(venues) {
+          if (venues.length > 0) {
+            vm.venue = venues[0];
+          }
+        });
+      }      
     }
 })();
